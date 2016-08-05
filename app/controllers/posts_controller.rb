770c820493c467044e14ad0de63a5777a966e59b
alejandro <alexghoro@outlook.com>
2016-08-05 10:05:59 -0400

    add edit security to the posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :owned_post, only: [:edit, :update, :destroy]
 
 	def index
     @posts = Post.all
@@ -63,5 +64,12 @@ class PostsController < ApplicationController
   def set_post
     @post = Post.find(params[:id])
   end
+
+  def owned_post 
+    unless current_user == @post.user
+      flash[:alert] = "Invalid url!"
+      redirect_to root_path
+    end
+  end 
 
 end
